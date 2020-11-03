dd68d62f8e94d110fdb9ba86b7f036b1660e12b0
toonyutthasak <49738920+toonyutthasak@users.noreply.github.com>
2020-11-03 22:36:16 +0800

    Add files via upload

---
+++ src/styles/colors.js
@@ -0,0 +1,54 @@
+const blueVersion = {
+  primary: '#555CC4',
+  primaryLight: '#829BF8',
+  primaryGradientStart: '#4f44b6',
+  primaryGradientEnd: '#4f44b6',
+  secondaryGradientStart: '#FF1358',
+  secondaryGradientEnd: '#FF1358',
+  profileGradientStart: '#54CBF6',
+  profileGradientEnd: '#49D2D0',
+  secondary: '#FF1358',
+  grey: '#acacac',
+  gray: '#5f5f5f',
+  darkGray: '#4d4d4d',
+  lightGray: '#9b9b9b',
+  white: '#ffffff',
+  blue: '#5A81F7',
+  bluish: '#F1F1F7',
+  black: '#000000',
+  green: '#6DD0A3',
+  yellow: '#ffc247',
+
+  /*
+ * @Author: Wooden Lim 
+ * @Date: 2019-12-18 09:58:19 
+ * @Last Modified by: Wooden Lim
+ * @Last Modified time: 2020-09-02 22:29:52
+ * @Change Log: 
+ * 1. Added Color code
+ */
+
+  p1: "#D31145", p2: "#596C80", p3: "#554344", p4: "#363E3F",
+
+  s1: '#E86487', s2: '#FBBF56', s3: '#97CB5D', s4: '#46D3BD', s5: '#22A8DA', s6: '#9962D2',
+
+  t1: '#F6CCD7', t2: '#B8123E', t3: '#F9E1B8', t4: '#C36418', t5: '#C6EB9E', t6: '#65962E', 
+  t7: '#A6E8DE', t8: '#158D7A', t9: '#A9DCEF', t10: '#0A6E93', t11: '#D7BFF0', t12: '#551893',
+
+  b1: '#D4EDF1', b2: '#FAF9F3', b3: '#F0EEE1', b4: '#DBDAD4', b5: '#B3B1A8', b6: '#6F6E68',
+  b7: '#43423E', b8: '#302F2C', b9: '#292025',
+
+  k: '#000000', w: '#FFFFFF',
+
+  u1: '#FF0000', u2: '#FF5500', u3: '#405B70', u4: '#769FAB',
+
+  grey: '#CBCBCB',
+
+  //Vitality Status Color
+  g2: ['#F15A22', '#FBC49D'],
+  g3: ['#B3B1A8', '#DBDAD3'],
+  g4: ['#F7981F', '#FFD855'],
+  g5: ['#27AAE1', '#98E0FF'],
+};
+
+export default blueVersion;
